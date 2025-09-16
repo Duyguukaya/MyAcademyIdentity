123dddbb28da25f80eac3fe166fca64290acad09
duygukayaa <dygkaya56@gmail.com>
2025-09-16 18:15:43 +0300

    son 3 mesajın listelenmesi tamamlandı

---
+++ EmailApp/ViewComponents/_LatestMessagesComponent.cs
@@ -0,0 +1,34 @@
+﻿using EmailApp.Context;
+using EmailApp.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailApp.ViewComponents
+{
+    public class _LatestMessagesComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public _LatestMessagesComponent(AppDbContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var latestMessages = await _context.Messages
+                .Where(m => m.ReceiverId == user.Id && !m.is_deleted) // silinmemiş mesajlar
+                .OrderByDescending(m => m.SendDate)
+                .Take(3)
+                .Include(m => m.Sender)
+                .ToListAsync();
+
+            return View(latestMessages);
+        }
+    }
+}
